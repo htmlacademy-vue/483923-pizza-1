3c251fb169f0bac90269f90cc934ce6a36e65712
Lara87 <lara.87@bk.ru>
2022-09-13 14:27:17 +0400

    add file SelectorItem

---
+++ src/frontend/src/common/components/SelectorItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <AppDrag :transfer-data="ingredient" :draggable="draggable">
+        <span :class="draggable && 'item--draggable'">
+            {{ingredient.name}}
+        </span>
+    </AppDrag>
+</template>
+
+<script>
+import AppDrag from './AppDrag.vue';
+import { INGREDIENT_MAX_COUNT } from '../constants';
+
+export default {
+    name: "SelectorItem",
+    components: {
+        AppDrag
+    },
+    props: {
+        ingredient: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        draggable() {
+            return this.ingredient.count < INGREDIENT_MAX_COUNT;  
+        },
+    },
+};
+</script>
