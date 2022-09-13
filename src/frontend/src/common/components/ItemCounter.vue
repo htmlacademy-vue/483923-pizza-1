99c747ff6a5cc1b8cae400d916912eb0d55a5fcb
Lara87 <lara.87@bk.ru>
2022-09-13 14:12:41 +0400

    add file ItemCounter.vue

---
+++ src/frontend/src/common/components/ItemCounter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="counter counter--orange ingredients__counter">
+    <button
+      type="button"
+      class="counter__button counter__button--minus"
+      @click="$emit('decrementCount', ingredient.id)"
+      :disabled="isMinDisabled"
+    >
+      <span class="visually-hidden">Меньше</span>
+    </button>
+    <input
+      type="text"
+      name="counter"
+      class="counter__input"
+      :value="ingredient.count"
+      readonly
+    />
+    <button
+      type="button"
+      class="counter__button counter__button--plus"
+      @click="$emit('incrementCount', ingredient.id)"
+      :disabled="isMaxDisabled"
+    >
+      <span class="visually-hidden">Больше</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import { INGREDIENT_MIN_COUNT, INGREDIENT_MAX_COUNT } from "@/common/constants";
+
+export default {
+  name: "ItemCounter",
+  props: {
+    ingredient: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    isMinDisabled() {
+      return this.ingredient.count === INGREDIENT_MIN_COUNT;
+    },
+    isMaxDisabled() {
+      return this.ingredient.count === INGREDIENT_MAX_COUNT;
+    },
+  },
+};
+</script>
