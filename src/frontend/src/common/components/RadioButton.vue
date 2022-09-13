4646e7f0d21f266c5280d134e241c243614416a6
Lara87 <lara.87@bk.ru>
2022-09-13 23:46:10 +0400

    add radioButton

---
+++ src/frontend/src/common/components/RadioButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <label :class="radio">
+        <input type="radio" name="name" :value="value" :checked="checked" class="visually-hidden"
+            @change="$emit('changeRadio', $event.target.value)" />
+        <span>{{ title }}</span>
+    </label>
+</template>
+
+<script>
+    export default {
+        name: "RadioButton",
+        props:{
+            name:{
+                type: String,
+                required: true,
+                validator: v => v.length,
+            },
+            value:{
+                type: String,
+                required: true,
+                validator: v => v.length,
+            },
+            title:{
+                type: String,
+                required: true,
+                validator: v => v.length,
+            },
+            checked:{
+                type: Boolean,
+                default: false,
+            },
+        },
+    };
+</script>
